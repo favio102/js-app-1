74c569a9ab29856f48620088728422afde82450d
John Ploumistos <ploumistosjohn@gmail.com>
2023-05-03 13:37:42 +0300

    main game

--- app.js
+++ app.js
@@ -1,15 +1,99 @@
-const hands = ['rock', 'paper', 'scissors']
-const randomIndex = Math.floor(Math.random() * hands.length);
-const computerHand = hands[randomIndex];
-console.log(computerHand)
-
-let playerHand = " ";
-while (true) {
-  console.log("Please choose your hand" + "("+hands+")" + "\n>>")
-  playerHand = prompt("Enter ");
-  if (hands.includes(playerHand)) {
-    break;
-  }else{
-    prompt("Wrong choice....");
-  }
+function computerPlay() {
+    let computerPicks = ["rock", "paper", "scissors"];
+    let computer;
+    computer = Math.floor(Math.random() * computerPicks.length);
+    return computerPicks[computer];
 }
+
+function player() {
+    let playerPick;
+    let correctInputs = ["rock", "paper", "scissors"];
+    playerPick = prompt("Rock Paper Or Scissors").toLowerCase();
+    if (playerPick === "" || !correctInputs.includes(playerPick)) {
+        return -100;
+    }
+    return playerPick;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let resultRound;
+    if (playerSelection === -100) {
+        resultRound = { message: "Wrong Input", whoWon: -100 };
+    } else if (playerSelection === computerSelection) {
+        resultRound = { message: "Tie" };
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        resultRound = {
+            message: "You Lose! Paper beats Rock",
+            whoWon: "computer",
+        };
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        resultRound = {
+            message: "You Won! Rock beat Scissors",
+            whoWon: "player",
+        };
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        resultRound = { message: "You Won! Paper beat Rock", whoWon: "player" };
+    } else if (
+        playerSelection === "paper" &&
+        computerSelection === "scissors"
+    ) {
+        resultRound = {
+            message: "You Lose! Scissors beat Paper",
+            whoWon: "computer",
+        };
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        resultRound = {
+            message: "You Lose! Rock beats Scissors",
+            whoWon: "computer",
+        };
+    } else if (
+        playerSelection === "scissors" &&
+        computerSelection === "paper"
+    ) {
+        resultRound = {
+            message: "You Won! Scissors beat Paper",
+            whoWon: "player",
+        };
+    }
+    return resultRound;
+}
+
+function game() {
+    let resultNumber = 0;
+    let countRound = 1;
+    let result;
+    let rounds = 5;
+    let playerSelection;
+    let computerSelection;
+    while (rounds > 0) {
+        console.log(`== Round ${countRound++} == `);
+        playerSelection = player();
+        if (playerSelection === -100) {
+            console.log("Wrong Input⛔⛔");
+            break;
+        }
+        console.log(`You chose: ${playerSelection}`);
+        computerSelection = computerPlay();
+        console.log(`Computer chose: ${computerSelection}`);
+        result = playRound(playerSelection, computerSelection);
+        if (result.whoWon === "player") {
+            resultNumber++;
+            console.log(result.message);
+        } else if (result.whoWon === "computer") {
+            resultNumber--;
+            console.log(result.message);
+        } else {
+            console.log(result.message);
+        }
+        rounds--;
+    }
+    if (resultNumber > 0 && playerSelection !== -100) {
+        console.log("Hooraayyy!!!You Did It !!! SkyNet is Defeated !!🎉🎉 ");
+    } else if (resultNumber < 0 && playerSelection !== -100) {
+        console.log("SkyNet Wins The Game!! We are Doomed!!😢😢💥💥");
+    } else if (resultNumber === 0 && playerSelection !== -100) {
+        console.log("Its a Tie!! No one won!!➖➖");
+    }
+}
+
+game();
