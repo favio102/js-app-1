39817b485984d71d4549fc50ed07c48aa444932d
Favio Valero <favio102@protonmail.com>
2023-05-03 03:28:10 -0700

    initial code

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-window.alert("test ")
+const hands = ['rock', 'paper', 'scissors']
+const randomIndex = Math.floor(Math.random() * hands.length);
+const computerHand = hands[randomIndex];
+console.log(computerHand)
+
+let playerHand = " ";
+while (true) {
+  console.log("Please choose your hand" + "("+hands+")" + "\n>>")
+  playerHand = prompt("Enter ");
+  if (hands.includes(playerHand)) {
+    break;
+  }else{
+    prompt("Wrong choice....");
+  }
+}
